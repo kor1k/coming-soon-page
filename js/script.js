164a0a26fd5bf0b9440fa84a4cfd448de5024ca1
kor1k <koortttt@gmail.com>
2019-03-13 19:54:38 +0200

    @kor1k | added modal window

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ $(function () {
             Email: ${$("#form_email").val()}
             `
         };
-        console.log(data.message);
+
         $.ajax({
             type: "POST",
             url: "../php/email.php",
@@ -16,6 +16,23 @@ $(function () {
                 $('.success').fadeIn(1000);
             }
         });
+
+        $('#modal').addClass('open');
+        $('.overlay').addClass('show');
+
+        $("#form_phone").val('');
+        $("#form_email").val('');
+
+        $('.close, .overlay').click(function () {
+            $('.overlay').removeClass('show')
+            $('#modal').removeClass('open');
+        });
+
+
         return false;
     });
 });
+
+// Modal window Start
+
+// Modal window End
